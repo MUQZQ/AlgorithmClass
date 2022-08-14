80e8da056710eb2e5799ddd115458c8436d65790
MUQZQ <1019009484@qq.com>
2022-08-14 20:45:46 +0800

    名称：数组对数器 类名：LogarithmArray

---
+++ src/utils/LogarithmArray.java
@@ -0,0 +1,93 @@
+package utils;
+
+import java.util.Arrays;
+
+/**
+ * @description: 数组对数器
+ * @author: QZQ
+ * @date: 2022/8/14
+ **/
+public class LogarithmArray {
+    /**
+     * 排序对比器
+     *
+     * @param arr
+     */
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+
+    /**
+     * 生成随机数组
+     *
+     * @param maxSize
+     * @param maxValue
+     * @return
+     */
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) (Math.random() * (maxSize + 1))];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1)) - (int) (Math.random() * maxValue);
+        }
+        return arr;
+    }
+
+
+    /**
+     * 复制数组
+     *
+     * @param arr
+     * @return
+     */
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        System.arraycopy(arr, 0, res, 0, arr.length);
+        return res;
+    }
+
+
+    /**
+     * 对比数组一致性
+     *
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    private static boolean isEqual(int[] arr1, int[] arr2) {
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1 == null || arr2 == null) {
+            return false;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 打印数组
+     *
+     * @param arr
+     */
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int j : arr) {
+            System.out.print(j + ",");
+        }
+        System.out.println();
+    }
+}
