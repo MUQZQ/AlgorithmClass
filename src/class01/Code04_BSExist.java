b36f5c90b68b95ed7ab5f4a508692c394f86b0f4
MUQZQ <1019009484@qq.com>
2022-08-14 21:27:30 +0800

    名称：二分查找是否存在 类名：Code04_BSExist

--- src/class01/Code04_BSExist.java
+++ src/class01/Code04_BSExist.java
@@ -1,5 +1,9 @@
 package class01;
 
+import java.util.Arrays;
+
+import static utils.LogarithmArray.generateRandomArray;
+
 /**
  * @description: 二分查找是否存在
  * @author: QZQ
@@ -13,10 +17,11 @@ public class Code04_BSExist {
 
         int L = 0;
         int R = sortedArray.length - 1;
-        int mid = 0;
+        int mid;
 
         //L...R
         while (L < R) {// L..R 至少两个数的时候
+            mid = L + ((R - L) >> 1);
             if (sortedArray[mid] == target) {
                 return true;
             }
@@ -26,7 +31,7 @@ public class Code04_BSExist {
                 R = mid - 1;
             }
         }
-        return sortedArray[R] == target;//只有一个数的时候
+        return sortedArray[L] == target;//只有一个数的时候,取左边，不然有可能越界R=-1
     }
 
     //for test
@@ -42,4 +47,21 @@ public class Code04_BSExist {
         return false;
     }
 
+    public static void main(String[] args) {
+
+        int testTime = 5000;
+        int maxSize = 10;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            Arrays.sort(arr);
+            int value = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            if (comparator(arr, value) != exist(arr, value)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    }
 }
