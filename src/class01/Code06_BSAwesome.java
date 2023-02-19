6b665cce1a6268da1d32c204534fa82d04b573a7
MUQZQ <1019009484@qq.com>
2023-02-20 00:18:17 +0800

    描述：codeCheck 类名：Code06_BSAwesome

--- src/class01/Code06_BSAwesome.java
+++ src/class01/Code06_BSAwesome.java
@@ -6,6 +6,37 @@ package class01;
  * @date: 2022/8/14
  **/
 public class Code06_BSAwesome {
+    //for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 30;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize);
+            int ans = getLessIndex(arr);
+            if (!isRight(arr, ans)) {
+                System.out.println("出错了！");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+    /**
+     * 随机生成相邻不相等的数组
+     *
+     * @param maxSize
+     * @return
+     */
+    public static int[] generateRandomArray(int maxSize) {
+        int[] arr = new int[(int) (Math.random() * (maxSize + 1))];
+        for (int i = 1; i < arr.length; i++) {
+            do arr[i] = (int) ((int) (Math.random() * (maxSize + 1)) - Math.random() * maxSize);
+            while (arr[i] == arr[i - 1]);
+        }
+        return arr;
+    }
+
     public static int getLessIndex(int[] arr) {
         int len = arr.length;
         if (len == 0) {
@@ -46,38 +77,5 @@ public class Code06_BSAwesome {
             return arr[index] < arr[index - 1];
         }
         return arr[index] < arr[index - 1] && arr[index] < arr[index + 1];
-    }
-
-    /**
-     * 随机生成相邻不相等的数组
-     *
-     * @param maxSize
-     * @param maxValue
-     * @return
-     */
-    public static int[] generateRandomArray(int maxSize, int maxValue) {
-        int[] arr = new int[(int) (Math.random() * (maxSize + 1))];
-        for (int i = 1; i < arr.length; i++) {
-            do arr[i] = (int) ((int) (Math.random() * (maxSize + 1)) - Math.random() * maxSize);
-            while (arr[i] == arr[i - 1]);
-        }
-        return arr;
-    }
-
-    //for test
-    public static void main(String[] args) {
-        int testTime = 500000;
-        int maxSize = 30;
-        int maxValue = 100;
-        System.out.println("测试开始");
-        for (int i = 0; i < testTime; i++) {
-            int[] arr = generateRandomArray(maxSize, maxValue);
-            int ans = getLessIndex(arr);
-            if (!isRight(arr, ans)) {
-                System.out.println("出错了！");
-                break;
-            }
-        }
-        System.out.println("测试结束");
     }
 }
