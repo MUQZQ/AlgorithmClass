45764e61db9f1afb976d056a0999cec7792e859d
MUQZQ <1019009484@qq.com>
2023-02-20 00:39:04 +0800

    描述：本题测试链接 :493. 翻转对 类名：Code04_BiggerThanRightTwice

---
+++ src/class04/Code04_BiggerThanRightTwice.java
@@ -0,0 +1,100 @@
+package class04;
+
+import static utils.LogarithmArray.*;
+
+/**
+ * 在一个数组中，
+ * 对于每个数num，求有多少个后面的数 * 2 依然<num，求总个数
+ * 比如：[3,1,7,0,2]
+ * 3的后面有：1，0
+ * 1的后面有：0
+ * 7的后面有：0，2
+ * 0的后面没有
+ * 2的后面没有
+ * 所以总共有5个
+ *
+ * @description:本题测试链接 : <a href="https://leetcode.com/problems/reverse-pairs/">...</a>
+ * @author: QZQ
+ * @date: 2023/2/19
+ **/
+public class Code04_BiggerThanRightTwice {
+    public static int reversePairs(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return 0;
+        }
+        return process(arr, 0, arr.length - 1);
+    }
+
+    private static int process(int[] arr, int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int mid = l + ((r - l) >> 1);
+        return process(arr, l, mid) + process(arr, mid + 1, r) + merge(arr, l, mid, r);
+    }
+
+    private static int merge(int[] arr, int l, int mid, int r) {
+        // [L,M][M+1,R]
+        int ans = 0;
+
+        // 目前囊括进来的数，是从[mid+1,windowR)
+        int windowR = mid + 1;
+        for (int i = l; i <= mid; i++) {
+            while (windowR <= r && arr[i] > arr[windowR] * 2) {
+                windowR++;
+            }
+            ans += windowR - mid - 1;
+        }
+
+        // 聚合
+        int i = 0;
+        int p1 = l;
+        int p2 = mid + 1;
+        int[] help = new int[r - l + 1];
+        while (p1 <= mid && p2 <= r) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= mid) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+        return ans;
+    }
+
+    // for test
+    public static int comparator(int[] arr) {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > (arr[j] << 1)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            if (reversePairs(arr1) != comparator(arr2)) {
+                System.out.println("Oops!");
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+}
+
