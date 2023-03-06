0d8f59c7c11902f1bcae6d1d84155da480c56ce1
MUQZQ <1019009484@qq.com>
2023-03-07 07:47:06 +0800

    描述：本题测试链接 :327. 区间和的个数 类名：Code01_CountOfRangSum

--- src/class05/Code01_CountOfRangSum.java
+++ src/class05/Code01_CountOfRangSum.java
@@ -7,21 +7,29 @@ package class05;
  * @date: 2023/3/6
  **/
 public class Code01_CountOfRangSum {
-    public static int countRangSum(int[] nums, int lower, int upper) {
+    public static void main(String[] args) {
+        int[] nums = {-2, 5, -1};
+        int lower = -2;
+        int upper = 2;
+        System.out.println(countRangeSum(nums, lower, upper));
+    }
+
+    public static int countRangeSum(int[] nums, int lower, int upper) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
+        // 前缀和数组
         long[] sum = new long[nums.length];
         sum[0] = nums[0];
-
         for (int i = 1; i < nums.length; i++) {
             sum[i] = sum[i - 1] + nums[i];
         }
+
         return process(sum, 0, sum.length - 1, lower, upper);
     }
 
     private static int process(long[] sum, int L, int R, int lower, int upper) {
-        // 递归头，当只有一个元素是跳出
+        // baseCase当只有一个元素,前缀和在区间[lower,upper]
         if (L == R) {
             return sum[L] >= lower && sum[L] <= upper ? 1 : 0;
         }
@@ -30,40 +38,40 @@ public class Code01_CountOfRangSum {
         return process(sum, L, mid, lower, upper) + process(sum, mid + 1, R, lower, upper) + merger(sum, L, mid, R, lower, upper);
     }
 
-    private static int merger(long[] arr, int l, int mid, int r, int lower, int upper) {
+    private static int merger(long[] sum, int L, int M, int R, int lower, int upper) {
         int ans = 0;
-        int windowL = l;
-        int windowR = r;
+        int windowL = L;
+        int windowR = L;
 
         // [windowL,windowR)
-        for (int i = mid + 1; i <= r; i++) {
-            long min = arr[i] - upper;
-            long max = arr[i] - lower;
-            while (windowR <= mid && arr[windowR] <= max) {
+        for (int i = M + 1; i <= R; i++) { // 有数组遍历，前缀和转换目标范围
+            long min = sum[i] - upper;
+            long max = sum[i] - lower;
+            while (windowR <= M && sum[windowR] <= max) {
                 windowR++;
             }
-            while (windowL <= mid && arr[windowL] < min) {
+            while (windowL <= M && sum[windowL] < min) {
                 windowL++;
             }
-            ans += windowR - windowL;
+            ans += windowR - windowL; //
         }
 
         // 数据结构merge部分
-        long[] help = new long[r - l + 1];
+        long[] help = new long[R - L + 1];
         int i = 0;
-        int p1 = l;
-        int p2 = mid + 1;
-        while (p1 <= mid && p2 <= r) {
-            help[i] = arr[p1] > arr[p2] ? arr[p2++] : arr[p1++];
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = sum[p1] > sum[p2] ? sum[p2++] : sum[p1++];
         }
-        while (p1 <= mid) {
-            help[i++] = arr[p1++];
+        while (p1 <= M) {
+            help[i++] = sum[p1++];
         }
-        while (p2 <= r) {
-            help[i++] = arr[p2++];
+        while (p2 <= R) {
+            help[i++] = sum[p2++];
         }
         //每次排完序，都要将原数组排序部分刷新
-        System.arraycopy(help, 0, arr, l, help.length);
+        System.arraycopy(help, 0, sum, L, help.length);
         return ans;
     }
 }
