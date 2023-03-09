2ec4613e4ffe7c60957a40280811738a0aff4770
MUQZQ <1019009484@qq.com>
2023-03-09 23:19:11 +0800

    描述：本题测试链接 :快排递归和非递归（队列、栈）的实现 类名：Code03_QuickSortRecursiveAndUnrecursive

---
+++ src/class05/Code03_QuickSortRecursiveAndUnrecursive.java
@@ -0,0 +1,156 @@
+package class05;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import static utils.LogarithmArray.*;
+
+/**
+ * @author Qin
+ */
+public class Code03_QuickSortRecursiveAndUnrecursive {
+
+    /**
+     * 荷兰国旗问题
+     */
+    public static int[] netherlandsFlag(int[] arr, int L, int R) {
+        if (L > R) {
+            return new int[]{-1, -1};
+        }
+        if (L == R) {
+            return new int[]{L, R};
+        }
+        int less = L - 1;
+        int more = R;
+        int index = L;
+        while (index < more) {
+            if (arr[index] == arr[R]) {
+                index++;
+            } else if (arr[index] < arr[R]) {
+                swap(arr, index++, ++less);
+            } else {
+                swap(arr, index, --more);
+            }
+        }
+        swap(arr, more, R);
+        return new int[]{less + 1, more};
+    }
+
+
+    /**
+     * 快排递归版本
+     */
+    public static void quickSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    public static void process(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        swap(arr, L + (int) (Math.random() * (R - L + 1)), R);
+        int[] equalArea = netherlandsFlag(arr, L, R);
+        process(arr, L, equalArea[0] - 1);
+        process(arr, equalArea[1] + 1, R);
+    }
+
+    /**
+     * 快排3.0 非递归版本 用栈来执行
+     */
+    public static void quickSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        swap(arr, (int) (Math.random() * N), N - 1);
+        int[] equalArea = netherlandsFlag(arr, 0, N - 1);
+        int el = equalArea[0];
+        int er = equalArea[1];
+        Stack<Op> stack = new Stack<>();
+        stack.push(new Op(0, el - 1));
+        stack.push(new Op(er + 1, N - 1));
+        while (!stack.isEmpty()) {
+            Op op = stack.pop(); // op.l ... op.r
+            if (op.l < op.r) {
+                swap(arr, op.l + (int) (Math.random() * (op.r - op.l + 1)), op.r);
+                equalArea = netherlandsFlag(arr, op.l, op.r);
+                el = equalArea[0];
+                er = equalArea[1];
+                stack.push(new Op(op.l, el - 1));
+                stack.push(new Op(er + 1, op.r));
+            }
+        }
+    }
+
+    /**
+     * 快排3.0 非递归版本 用队列来执行
+     */
+    public static void quickSort3(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        swap(arr, (int) (Math.random() * N), N - 1);
+        int[] equalArea = netherlandsFlag(arr, 0, N - 1);
+        int el = equalArea[0];
+        int er = equalArea[1];
+        Queue<Op> queue = new LinkedList<>();
+        queue.offer(new Op(0, el - 1));
+        queue.offer(new Op(er + 1, N - 1));
+        while (!queue.isEmpty()) {
+            Op op = queue.poll();
+            if (op.l < op.r) {
+                swap(arr, op.l + (int) (Math.random() * (op.r - op.l + 1)), op.r);
+                equalArea = netherlandsFlag(arr, op.l, op.r);
+                el = equalArea[0];
+                er = equalArea[1];
+                queue.offer(new Op(op.l, el - 1));
+                queue.offer(new Op(er + 1, op.r));
+            }
+        }
+    }
+
+    /**
+     * 跑大样本随机测试（对数器）
+     */
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            int[] arr3 = copyArray(arr1);
+            quickSort1(arr1);
+            quickSort2(arr2);
+            quickSort3(arr3);
+            if (!isEqual(arr1, arr2) || !isEqual(arr1, arr3)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println("test end");
+        System.out.println("测试" + testTime + "组是否全部通过：" + (succeed ? "是" : "否"));
+    }
+
+    /**
+     * 快排非递归版本需要的辅助类
+     * 要处理的是什么范围上的排序
+     */
+    public static class Op {
+        public int l;
+        public int r;
+
+        public Op(int left, int right) {
+            l = left;
+            r = right;
+        }
+    }
+
+}
